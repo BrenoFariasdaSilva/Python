1f74a55665672219c3e7acde15724c7e3a28458d
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-18 13:06:36 -0300

    Updating the shellscript for the dagster tutorial

--- Dagster/tutorial-project/installation.sh
+++ Dagster/tutorial-project/installation.sh
@@ -6,11 +6,5 @@
 
 pip install dagster dagit requests pandas matplotlib wordcloud dagster_duckdb dagster_duckdb_pandas
 
-# Create a new project
-dagster project scaffold --name tutorial-project
-
-# Open the project folder
-cd tutorial-project
-
 # Run the project in dev mode
 dagster dev
