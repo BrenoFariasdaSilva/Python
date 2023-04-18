0f2a6932dc6cd3fcb58092e86b65f4ea80ddea58
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-04-18 12:33:39 -0300

    shell script for running the dagster

---
+++ dagster.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Run:
+# chmod +x dagster.sh
+# sudo ./dagster.sh
+
+pip install dagster dagit requests pandas matplotlib wordcloud dagster_duckdb dagster_duckdb_pandas
+
+# Create a new project
+dagster project scaffold --name tutorial-project
+
+# Open the project folder
+cd tutorial-project
+
+# Run the project in dev mode
+dagster dev
