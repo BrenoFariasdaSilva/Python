87192583913b0caf64231141b2a0993220490e52
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-19 09:05:32 -0300

    Adding comments to Non argument dependencies

--- Dagster/tutorial-project/tutorial_project/non-argument-dependencies.py
+++ Dagster/tutorial-project/tutorial_project/non-argument-dependencies.py
@@ -7,3 +7,7 @@ def upstream_asset() -> None:
 @asset(non_argument_deps={"upstream_asset"})
 def downstream_asset() -> None:
     execute_query("CREATE TABLE shopping_list AS SELECT * FROM sugary_cereals")
+    
+# In this example, Dagster doesn’t need to load data from upstream_asset to successfully compute the downstream_asset.
+# While downstream_asset does depend on upstream_asset, the key difference with non_argument_deps is that data isn’t being passed between the functions.
+# Specifically, the data from the sugary_cereals table isn't being passed as an argument to downstream_asset.
