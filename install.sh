450d061ddccef59fb5735ededb2062b17ae361a2
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-08 09:26:46 -0300

    REFACTOR: Updating installation shellscript

--- install.sh
+++ install.sh
@@ -4,4 +4,15 @@
 # chmod +x install.sh
 # sudo ./install.sh
 
+# Python3
 sudo apt-get install python3 python3-venv python3-pip -y
+
+# Visual Studio Code
+sudo apt update -y
+sudo apt install software-properties-common apt-transport-https cd ~/Downloads
+wget -y
+cd ~/Downloads
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update -y
+sudo apt install code -y
