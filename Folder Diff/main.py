bbcafd157aaf4c8b0be0e5b11972c371a6e41dc1
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 13:57:18 -0300

    REFACTOR: Updating the Python Folder Diff Algorithm

--- Folder Diff/main.py
+++ Folder Diff/main.py
@@ -1,30 +1,46 @@
-import os
+import os # For walking through the directory tree
+from colorama import Style # For coloring the terminal
 
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
+	CLEAR_TERMINAL = "\033[H\033[J" # Clear the terminal
+
+# This function will return the difference between two folders
 def diff(paths):
-	first_path = paths[0]
-	second_path = paths[1]
+	first_path = paths[0] # The first path
+	second_path = paths[1] # The second path
  
-	first_path_folders = os.listdir(first_path)
-	second_path_folders = os.listdir(second_path)
+	first_path_folders = os.listdir(first_path) # Get the folders in the first path
+	second_path_folders = os.listdir(second_path) # Get the folders in the second path
  
-	diff_folders = []
+	diff_folders = [] # The difference between the folders
  
+	# Get the difference between the folders
 	for folder in first_path_folders:
-		if folder not in second_path_folders:
-			diff_folders.append(folder)
+		if folder not in second_path_folders: # If the folder is not in the second path
+			diff_folders.append(folder) # Add the folder to the list
    
-	return diff_folders
+	return diff_folders # Return the difference between the folders
 
+# This is the main function
 def main():
-	paths = []
+	paths = [] # List of paths
  
+	# Get the paths
 	for i in range(2):
-		path = input("Enter path: ")
-		paths.append(path)
+		path = input(f"{backgroundColors.GREEN}Enter path: {Style.RESET_ALL}") # Get the path
+		paths.append(path) # Add the path to the list
   
+	# Get the difference between the folders
 	diff_folders = diff(paths)
  
-	[ print(f"{diff_folders[i]}: not in {paths[2]}") for i in range(len(diff_folders)) ]
+	# Print the difference
+	[ print(f"{backgroundColors.CYAN}{diff_folders[i]}{backgroundColors.GREEN}: not in {backgroundColors.CYAN}{paths[2]}{Style.RESET_ALL}") for i in range(len(diff_folders)) ]
 
-if __name__ == "__main__":
-	main()
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+	main() # Call the main function
