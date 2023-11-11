9ca2cd7df2bc4fe918db6edc25f7366f5be40636
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 09:34:57 -0300

    REFACTOR: Updating the Word2Vec getTitles.py Algorithm

--- Word2Vector/getTitles.py
+++ Word2Vector/getTitles.py
@@ -1,21 +1,41 @@
-import json
+import json # Import the json module
+from tqdm import tqdm # For the progress bar
+from colorama import Style # For coloring the terminal
 
-def getTitles(qnt):
-	file = open('./jabref/issues.json', encoding="utf8") # Insert here the path to the issues.json file
-	data = json.load(file)
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
 
-	if qnt == 0:
-		qnt = len(data)
+# This function returns the titles of the issues
+def getTitles(quantity, issues_filepath):
+	file = open(issues_filepath, encoding="utf8") # Open the json file
+	data = json.load(file) # Load the json file
 
-	words = []
+	if quantity == 0: # If the quantity is 0
+		quantity = len(data) # Set the quantity to the number of issues
 
-	print(f'Began Processing Titles')
-	for i in range(len(data)):
-		if i >= qnt:
-			break
-		currentIssue = data[i]['issue_data']
-		if currentIssue['title']:
-			currentIssue['title'] = currentIssue['title'].replace('/', ' ').replace('\n', ' ').strip().lower()
-			words.append(currentIssue['title'])
-	print(f'Finished Processing Titles')
-	return words 
+	words = [] # Initialize the list of words
+
+	# For each issue in the json file
+	print(f"{backgroundColors.GREEN}Quantity: {backgroundColors.CYAN}{quantity}{Style.RESET_ALL}")
+	print(f"{backgroundColors.GREEN}Began Processing Titles{Style.RESET_ALL}")
+
+	# For each issue in the json file
+	for i in tqdm(range(len(data)), desc=f"{backgroundColors.GREEN}Processing Issues{Style.RESET_ALL}", unit="item"):
+		if i >= quantity: # If the quantity of issues to read has been reached
+			break # Stop reading issues
+		
+		currentIssue = data[i]["issue_data"] # Get the issue data
+
+		# If the issue has a title
+		if currentIssue["title"]: 
+			# Add the title to the list of words
+			currentIssue["title"] = currentIssue["title"].replace("/", " ").replace("\n", " ").strip().lower()
+			words.append(currentIssue["title"])
+
+	print(f"{backgroundColors.GREEN}Finished Processing Titles{Style.RESET_ALL}")
+
+	return words # Return the list of words
