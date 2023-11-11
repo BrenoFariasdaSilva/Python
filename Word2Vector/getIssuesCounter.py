667a9b4c194659b3d5c05345d1721edc8ee86bf2
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 09:35:30 -0300

    REFACTOR: Updating the Word2Vec getIssuesCounter.py Algorithm

--- Word2Vector/getIssuesCounter.py
+++ Word2Vector/getIssuesCounter.py
@@ -1,15 +1,18 @@
-import json
+import json # Import the json module
 
-def getIssuesCounter():
-	file = open('./jabref/issues.json')
-	data = json.load(file)
+# This function returns the number of issues that have a title
+def getIssuesCounter(issues_filepath):
+	file = open(issues_filepath, encoding="utf8") # Open the json file
+	data = json.load(file) # Load the json file
 
-	numberOfIssues = 0
+	numberOfIssues = 0 # Initialize the number of issues with a title
 
+	# For each issue in the json file
 	for i in range(len(data)):
-		currentIssue = data[i]['issue_data']
+		currentIssue = data[i]["issue_data"] # Get the issue data
 
-		if currentIssue['title']:
-			numberOfIssues += 1
+		# If the issue has a title
+		if currentIssue["title"]:
+			numberOfIssues += 1 # Increment the number of issues with a title
 	
-	return numberOfIssues 
+	return numberOfIssues  # Return the number of issues with a title
