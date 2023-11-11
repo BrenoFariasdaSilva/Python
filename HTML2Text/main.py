f161a587e9aadeb6c081afcf56e5a9c1263840c4
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 13:33:35 -0300

    REFACTOR: Updating the HTML2Text Python Algorithm

--- HTML2Text/main.py
+++ HTML2Text/main.py
@@ -1,48 +1,67 @@
-import html2text
-from urllib.request import urlopen
+import html2text # For convert html to text
+from urllib.request import urlopen # For open the url
+from colorama import Style # For coloring the terminal
 
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
+	CLEAR_TERMINAL = "\033[H\033[J" # Clear the terminal
+
+# This function get the url from the user input
 def user_input():
-	print(f"Choose type of input:")
-	print(f"1. Paste URL here")
-	print(f"2. Insert URL on code")
+	print(f"{backgroundColors.GREEN}Choose type of input:{Style.RESET_ALL}")
+	print(f"{backgroundColors.GREEN}1. Paste URL here{Style.RESET_ALL}")
+	print(f"{backgroundColors.GREEN}2. Insert URL on code{Style.RESET_ALL}")
  
 	url = "https://www.google.com" # Default url
-	option = int(input("Selected Input: "))
+	option = int(input(f"{backgroundColors.GREEN}Selected Input: {Style.RESET_ALL}")) # Get the option from the user input
 	
+	# Check if the option is valid
 	while option != 1 and option != 2:
-		print("Invalid option. Try again.")
+		print(f"{backgroundColors.YELLOW}Invalid option. Try again.{Style.RESET_ALL}")
   
+  	# Check the option
 	if option == 1:
-		url = input("Enter a URL: ")
+		url = input(f"{backgroundColors.GREEN}Enter a URL: {Style.RESET_ALL}")
 	elif option == 2: # If you choose the option "2" you need to change the url variable on the next line
 		url = "https://github.com/BrenoFariasdaSilva/Word2Vector"
 
-	return url
+	return url # Return the url
 
+# This function get the html file from the url
 def get_html_file(url):
-	html_file = urlopen(url) # open the url
-	html_as_text = html_file.read() # read the html file
-	return html_as_text
+	html_file = urlopen(url) # Open the url
+	html_as_text = html_file.read() # Read the html file
 
+	return html_as_text # Return the html file
+
+# This function search the string on the html file
 def search_string(string):
-	start_substring = "Algorithm i'm studying" # insert here the string that starts the text
-	end_substring = "json file." # insert here the string that ends the text
+	start_substring = "Algorithm i'm studying" # Insert here the string that starts the text
+	end_substring = "json file." # Insert here the string that ends the text
  
+	# Get the start and end index of the substring
 	start = string.find(start_substring)
 	end = string.find(end_substring)
 	substring = string[start:end+len(end_substring)]
-	return substring
 
+	return substring # Return the substring
+
+# This is the main function
 def main():
 	url = user_input() # get the url from the user input
 	html_file = get_html_file(url) # get the html file from the url
 	text_as_string = html2text.html2text(html_file.decode("utf-8"))
 	substring = search_string(text_as_string)
  
-	# command to clear the terminal
-	print("\033[H\033[J")
+	# Command to clear the terminal
+	print(f"{backgroundColors.CLEAR_TERMINAL}")
  
-	print (f"{substring}")
+	print(f"{backgroundColors.CYAN}{substring}{Style.RESET_ALL}") # Print the Substring
 
+# This is the standard boilerplate that calls the main() function.
 if __name__ == "__main__":
-	main()
+	main() # Call the main function
