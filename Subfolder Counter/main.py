7334f0fd6385c50197ac2a2129d7e046dd4328ea
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 10:01:01 -0300

    REFACTOR: Updating the SubFolders Counter Algorithm

--- Subfolder Counter/main.py
+++ Subfolder Counter/main.py
@@ -1,24 +1,34 @@
-import os
+import os # Import the os module for interacting with the operating system
+from colorama import Style # For coloring the terminal
 
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
+
+# This is the main function
 def main():
-   # ask user to enter paths to verify
-	while True:
-		path = input("Enter path to verify: ")
+	while True: 
+		# Ask user to enter paths to verify
+		path = input(f"{backgroundColors.GREEN}Enter path to verify: {Style.RESET_ALL}")
 		if path == "exit":
-			break
-		
-   
-	
-	for folder_name, subfolders, filenames in os.walk(path):
-		# ignore the path that is already in the path
-		if folder_name == path:
-			continue
-		if len(subfolders) > 0:
-			print(f"Folder: {folder_name}")
-			print(f"Subfolders: {subfolders}")
-			print(f"Files: {filenames}")
-			print()
+			break # Break the loop
 
+	# Verify if the path exists
+	for folder_name, subfolders, filenames in os.walk(path):
+		# Ignore the path that is already in the path
+		if folder_name == path:
+			continue # Continue to the next iteration
+
+		# Print the path, subfolders and files
+		if len(subfolders) > 0:
+			print(f"{backgroundColors.GREEN}Folder: {backgroundColors.CYAN}{folder_name}{Style.RESET_ALL}")
+			print(f"{backgroundColors.GREEN}Subfolders: {backgroundColors.CYAN}{subfolders}{Style.RESET_ALL}")
+			print(f"{backgroundColors.GREEN}Files: {backgroundColors.CYAN}{filenames}{Style.RESET_ALL}")
+			print(f"{backgroundColors.GREEN}")
+
+# This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-	main()
-	 
+	main() # Call the main() function
