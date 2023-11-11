ab856398a39fa7cd99a2d04be97c6a3b09afcbe7
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-11 10:07:17 -0300

    REFACTOR: Updating the SubFolders Steam App IDs Algorithm

--- SteamAppIDs/main.py
+++ SteamAppIDs/main.py
@@ -1,47 +1,72 @@
-# Create a program that open a url.txt file
-# Then, it wil search for every string that starts with "data-appid=\" and ends with ".\""
-# Then, it will save all the appid's in a file called appid.txt, where each appid is in a new line
+# This program extracts the appids from the Steam URL file and writes them to a text file.
 
-start_string = '<a href="https://steamcommunity.com/app/'
-end_string = '">'
-stop_string = '>12.0h<'
-PlayGamesLimit = 32
+from colorama import Style # For coloring the terminal
 
-def main():
-   TotalGamesFound = 0
-   file = open("url.txt", encoding="utf8")
-   lines = file.readlines()
-   file.close()
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
+
+# This function reads the URL file and returns the lines
+def read_url_file(file_path):
+   # Read the URL file
+   with open(file_path, encoding="utf8") as file:
+      lines = file.readlines() # Read the lines
+   return lines # Return the lines
+
+# This function extracts the appids from the lines
+def extract_appids(lines, start_string, end_string, stop_string, play_games_limit):
+   total_games_found = 0 # Total games found
+   appids = [] # List of appids
+
+   # Extract the appids from the lines
    for line in lines:
-      start = line.find(start_string)
-      end = line.find(end_string)
-      if start != -1 and end != -1:
-         TotalGamesFound += 1
-         if TotalGamesFound == 1:
-            appIDsFile = open("appid.txt", "w")
-            appIDsFile.write("")
-         appid = line[start + len(start_string):end]
-         appIDsFile = open("appid.txt", "a")
-         if (TotalGamesFound % PlayGamesLimit) == 0 and TotalGamesFound != 1:
-            appIDsFile.write(appid + "\n")
-         else:
-            appIDsFile.write(appid + ",")
+      start = line.find(start_string) # Find the start string
+      end = line.find(end_string) # Find the end string
+
+      if start != -1 and end != -1: # If the start and end strings are found
+         total_games_found += 1 # Increment the total games found
+         appid = line[start + len(start_string):end] # Extract the appid
+         appids.append(appid) # Append the appid to the list
+
+         # If the total games found is equal to the play games limit
+         if total_games_found % play_games_limit == 0 and total_games_found != 1:
+               yield ",".join(appids) # Yield the appids
+               appids = [] # Reset the list of appids
+
+      # If the stop string is found
       if line.find(stop_string) != -1:
-         appIDsFile.close()
-         # deleteLastChar()
-         break
-   print("Total Games Found: " + str(TotalGamesFound))
-         
-def deleteLastChar():
-   appIDsFile = open("appid.txt", "r")
-   lines = appIDsFile.readlines()
-   # if the last char in the last line is a comma, delete it
-   if lines[-1][-1] == ",":
-      lines[-1] = lines[-1][:-1]
-   appIDsFile = open("D:\Backup\Mega Sync\My Softwares\Windows\Steam ID File\Parzival\ParzivalIDs.txt", "w")
-   appIDsFile.write("")
-   appIDsFile.writelines(lines)
-   appIDsFile.close()
-               
-if __name__ == "__main__":
-   main()
+         break # Break the loop
+
+   return total_games_found # Return the total games found
+
+# This function writes the appids to a file
+def write_appids_to_file(appids, file_path):
+   with open(file_path, "w") as appids_file: # Open the file
+      appids_file.write("\n".join(appids)) # Write the appids to the file
+
+# This is the main function
+def main():
+   url_file_path = "url.txt"
+   appids_file_path = "appid.txt"
+   start_string = '<a href="https://steamcommunity.com/app/'
+   end_string = '">'
+   stop_string = '>12.0h<'
+   play_games_limit = 32
+
+   # Read the URL file
+   lines = read_url_file(url_file_path)
+   # Extract the appids from the lines
+   total_games_found = extract_appids(lines, start_string, end_string, stop_string, play_games_limit)
+
+   # Print the total games found
+   if total_games_found > 0:
+      print(f"{backgroundColors.GREEN}Total Games Found: {backgroundColors.CYAN}{total_games_found}{Style.RESET_ALL}")
+   else:
+      print(f"{backgroundColors.GREEN}No games found in the provided URL file.{Style.RESET_ALL}")
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+	main() # Call the main() function
