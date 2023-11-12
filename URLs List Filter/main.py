6f31169dbedbc242db4d6445cb30e50abeff3a47
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2023-11-12 15:26:54 -0300

    REFACTOR: Updating URLs List Filter Main Function Call Code

--- URLs List Filter/main.py
+++ URLs List Filter/main.py
@@ -70,6 +70,6 @@ def main():
 
 	print(f"{BackgroundColors.GREEN}Program finished.{Style.RESET_ALL}")
 
-# Description: This is the standard boilerplate that calls the main function.
-if __name__ == "__main__":
-	main() # Call the main function
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+   main() # Call the main function
