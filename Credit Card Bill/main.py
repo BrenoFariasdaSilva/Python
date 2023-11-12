d4a38b814c27fe31074a27a18caf40edb209cb01
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2023-11-12 15:24:02 -0300

    REFACTOR: Updating Credit Card Maun Function Call

--- Credit Card Bill/main.py
+++ Credit Card Bill/main.py
@@ -78,6 +78,6 @@ def main():
 	# Print the total sum of the "Valor" column
 	print(f"{BackgroundColors.GREEN}Total sum of the \"Valor\" column: {BackgroundColors.CYAN}R$ {df['Valor'].sum():.2f}{BackgroundColors.GREEN}.{Style.RESET_ALL}")
  
-# @brief: This is the standard boilerplate that calls the main() function.
-if __name__ == "__main__":
-	main()
+# This is the standard boilerplate that calls the main() function
+if __name__ == '__main__':
+	main() # Call the main function
