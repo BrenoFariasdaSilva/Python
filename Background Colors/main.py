f764d5e69b61d379f4bd2525f53b94e28405988a
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2023-11-12 14:55:15 -0300

    FEAT: Adding the Background Colors Code

---
+++ Background Colors/main.py
@@ -0,0 +1,11 @@
+from colorama import Style  # For coloring the terminal
+
+# Macros:
+class BackgroundColors:  # Colors for the terminal
+   CYAN = "\033[96m"  # Cyan
+   GREEN = "\033[92m"  # Green
+   YELLOW = "\033[93m"  # Yellow
+   RED = "\033[91m"  # Red
+   BOLD = "\033[1m"  # Bold
+   UNDERLINE = "\033[4m"  # Underline
+   CLEAR_TERMINAL = "\033[H\033[J"  # Clear the terminal
