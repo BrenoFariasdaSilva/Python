93f84b7e456e7c817febceae78cc872b07a63101
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2024-01-21 13:07:18 -0300

    FEAT: Adding the Compounds Interest Calculator Function to the Compound Interest Calculator

--- Compound Interest Calculator/main.py
+++ Compound Interest Calculator/main.py
@@ -81,6 +81,19 @@ def get_interest_rate(period_type):
 
    return interest_rate # Return the interest rate
 
+# This function calculates the compound interest
+def calculate_compound_interest(number_of_periods, initial_amount, regular_contribution, interest_rate):
+   total_amount = initial_amount # Initialize the total amount
+   total_amounts = [total_amount] # Initialize the list of the total amounts
+   periods = [0] # Initialize the list of the periods
+
+   for period in range(number_of_periods): # For each period
+      total_amount = total_amount * (1 + interest_rate / 100) + regular_contribution # Calculate the total amount
+      total_amounts.append(total_amount) # Add the total amount to the list of the total amounts
+      periods.append(period + 1) # Add the period to the list of the periods
+
+   return total_amounts, periods # Return the list of the total amounts and the list of the periods
+
 # This is the Main function
 def main():
    print(f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Compound Interest Calculator {BackgroundColors.GREEN}Algorithm!{Style.RESET_ALL}", end="\n\n") # Output the Welcome message
@@ -90,6 +103,8 @@ def main():
    initial_amount = get_initial_amount() # Get the initial amount from the user
    regular_contribution = get_regular_contribution(period_type) # Get the regular contribution from the user
    interest_rate = get_interest_rate(period_type) # Get the interest rate from the user
+
+   total_amounts, periods = calculate_compound_interest(number_of_periods, initial_amount, regular_contribution, interest_rate) # Calculate the compound interest
 
    print(f"\n{BackgroundColors.BOLD}{BackgroundColors.GREEN}Program finished.{Style.RESET_ALL}") # Output the end of the program message
 
