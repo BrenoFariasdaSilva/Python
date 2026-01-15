ffe51d15c83dad86512238142f9edf17f234d27f
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-01-15 14:13:46 -0300

    REFACTOR: Updating the Main Template Project

--- Main Template/main.py
+++ Main Template/main.py
@@ -106,21 +106,6 @@ def verbose_output(true_string="", false_string=""):
         print(false_string)  # Output the false statement string
 
 
-def calculate_execution_time(start_time, finish_time):
-    """
-    Calculates the execution time between start and finish times and formats it as hh:mm:ss.
-
-    :param start_time: The start datetime object
-    :param finish_time: The finish datetime object
-    :return: String formatted as hh:mm:ss representing the execution time
-    """
-
-    delta = finish_time - start_time  # Calculate the time difference
-    hours, remainder = divmod(delta.seconds, 3600)  # Calculate the hours, minutes and seconds
-    minutes, seconds = divmod(remainder, 60)  # Calculate the minutes and seconds
-    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # Format the execution time
-
-
 def verify_filepath_exists(filepath):
     """
     Verify if a file or folder exists at the specified path.
@@ -134,6 +119,21 @@ def verify_filepath_exists(filepath):
     )  # Output the verbose message
 
     return os.path.exists(filepath)  # Return True if the file or folder exists, False otherwise
+
+
+def calculate_execution_time(start_time, finish_time):
+    """
+    Calculates the execution time between start and finish times and formats it as hh:mm:ss.
+
+    :param start_time: The start datetime object
+    :param finish_time: The finish datetime object
+    :return: String formatted as hh:mm:ss representing the execution time
+    """
+
+    delta = finish_time - start_time  # Calculate the time difference
+    hours, remainder = divmod(delta.seconds, 3600)  # Calculate the hours, minutes and seconds
+    minutes, seconds = divmod(remainder, 60)  # Calculate the minutes and seconds
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # Format the execution time
 
 
 def play_sound():
