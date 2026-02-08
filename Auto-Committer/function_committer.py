669241fbc6789ff10d3ced01352d2b13f117b64e
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-02-08 16:12:50 -0300

    FEAT: Adding the play_sound function to function_committer.py

--- Auto-Committer/function_committer.py
+++ Auto-Committer/function_committer.py
@@ -107,6 +107,31 @@ RUN_FUNCTIONS = {
 # Functions Definitions:
 
 
+def play_sound():
+    """
+    Plays a sound when the program finishes and skips if the operating system is Windows.
+
+    :param: None
+    :return: None
+    """
+
+    current_os = platform.system()  # Get the current operating system
+    if current_os == "Windows":  # If the current operating system is Windows
+        return  # Do nothing
+
+    if verify_filepath_exists(SOUND_FILE):  # If the sound file exists
+        if current_os in SOUND_COMMANDS:  # If the platform.system() is in the SOUND_COMMANDS dictionary
+            os.system(f"{SOUND_COMMANDS[current_os]} {SOUND_FILE}")  # Play the sound
+        else:  # If the platform.system() is not in the SOUND_COMMANDS dictionary
+            print(
+                f"{BackgroundColors.RED}The {BackgroundColors.CYAN}{current_os}{BackgroundColors.RED} is not in the {BackgroundColors.CYAN}SOUND_COMMANDS dictionary{BackgroundColors.RED}. Please add it!{Style.RESET_ALL}"
+            )
+    else:  # If the sound file does not exist
+        print(
+            f"{BackgroundColors.RED}Sound file {BackgroundColors.CYAN}{SOUND_FILE}{BackgroundColors.RED} not found. Make sure the file exists.{Style.RESET_ALL}"
+        )
+
+
 def main():
     """
     Main function.
