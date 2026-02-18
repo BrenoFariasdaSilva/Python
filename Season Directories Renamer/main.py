f2b41db1d8ce8422c1a8b9bef0e64b9e1fa795ea
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-02-18 14:19:43 -0300

    REFACTOR: Updating the Library Imports in ./Season Directories Renamer\main.py

--- Season Directories Renamer/main.py
+++ Season Directories Renamer/main.py
@@ -52,10 +52,11 @@ import atexit  # For playing a sound when the program finishes
 import datetime  # For getting the current date and time
 import os  # For running a command in the terminal
 import platform  # For getting the operating system name
-import sys  # For system-specific parameters and functions
 from colorama import Style  # For coloring the terminal
-from Logger import Logger  # For logging output to both terminal and file
-from pathlib import Path  # For handling file paths
+import re  # For parsing directory names
+import requests  # For API requests
+from pathlib import Path  # For path handling
+from dotenv import load_dotenv  # For loading environment variables
 
 
 # Macros:
