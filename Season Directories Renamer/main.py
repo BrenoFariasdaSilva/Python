a030f98a13555d0b14ef06f32c4a687d212c26dc
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-02-18 14:19:32 -0300

    REFACTOR: Updating the main function in ./Season Directories Renamer\main.py

--- Season Directories Renamer/main.py
+++ Season Directories Renamer/main.py
@@ -235,12 +235,12 @@ def main():
     """
 
     print(
-        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Main Template Python{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
+        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Season Directory Renamer{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
         end="\n\n",
     )  # Output the welcome message
     start_time = datetime.datetime.now()  # Get the start time of the program
     
-    # Implement logic here
+    rename_dirs()  # Execute the directory renaming workflow
 
     finish_time = datetime.datetime.now()  # Get the finish time of the program
     print(
