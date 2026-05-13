88401c0353ffd1aec410657f7a1bd2c0a085825f
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-05-12 23:48:54 -0300

    REFACTOR: Updating the main function in ./main.py

--- Games Collection Manager/main.py
+++ Games Collection Manager/main.py
@@ -392,13 +392,13 @@ def main():
     """
 
     print(
-        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Main Template Python{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
-        end="\n\n",
+        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Games Collection Manager{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
+        end="\n",
     )  # Output the welcome message
     
     start_time = datetime.datetime.now()  # Get the start time of the program
     
-    # Implement logic here
+    process_all_txt_files()  # Discover, parse, normalize, and rewrite all TXT files in INPUTS_DIR
 
     finish_time = datetime.datetime.now()  # Get the finish time of the program
     
