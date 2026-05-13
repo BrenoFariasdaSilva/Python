b4019dd022b4519d996cc1d800677256f6ee635f
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-05-12 23:48:07 -0300

    REFACTOR: Updating the library imports in ./main.py

--- Games Collection Manager/main.py
+++ Games Collection Manager/main.py
@@ -50,6 +50,7 @@ import atexit  # For playing a sound when the program finishes
 import datetime  # For getting the current date and time
 import os  # For running a command in the terminal
 import platform  # For getting the operating system name
+import re  # For regular expression matching during game-line parsing
 import sys  # For system-specific parameters and functions
 from colorama import Style  # For coloring the terminal
 from Logger import Logger  # For logging output to both terminal and file
